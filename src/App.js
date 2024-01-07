dba7c24709bcf9eb7d3546a38c3ad00bf71fbd01
aliTalkhabi <atdes1999@gmail.com>
2024-01-07 21:55:38 +0330

    add MUI in project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 import View from './view/View';
 
