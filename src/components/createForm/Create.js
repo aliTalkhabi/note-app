d439d7dd84f88ab94e7177faa8b21bb1962f9c12
aliTalkhabi <atdes1999@gmail.com>
2024-01-15 23:12:50 +0330

    make MUI in Project and save data in right form from user

--- src/components/createForm/Create.js
+++ src/components/createForm/Create.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './create.module.css';
+import TitleInput from '../createForm/titleInput/TitleInput';
+
 const Create = () => {
     return (
         <div className={styles.formContainer}>
-
+            <TitleInput />
         </div>
     );
 };
---
+++ src/components/createForm/titleInput/TitleInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Box, TextField, Button } from '@mui/material';
+import styles from './TitileInput.module.css'
+
+const TitleInput = () => {
+    const [titleData, setTitleData] = useState('');
+    const [descriptionData, setDescriptionData] = useState('');
+    const setValueTitleData = (e) => {
+        setTitleData(e.target.value)
+    }
+    const setValueDescriptionData = (e) => {
+        setDescriptionData(e.target.value)
+    }
+    const saveData = () => {
+        console.log(`title: ${titleData}`);
+        console.log(`description: ${descriptionData}`);
+    }
+    return (
+        <>
+            <Box maxWidth='95%' height='10%' margin='10px auto'>
+                <TextField fullWidth value={titleData} label="Enter your title" type='text' color='primary' autoComplete='off' onChange={setValueTitleData} />
+
+            </Box>
+            <Box maxWidth='95%' height='75%' margin='10px auto'>
+                <TextField className={`${styles.input},${styles.label}`} value={descriptionData} fullWidth label="Enter your description" type='text' color='primary' style={{ 'height': '100%' }} autoComplete='off' onChange={setValueDescriptionData} />
+            </Box >
+            <Box maxWidth='95%' height='10%' margin='10px auto' display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                <Button variant="outlined" onClick={saveData}>Save</Button>
+            </Box>
+        </>
+    );
+};
+
+export default TitleInput;
